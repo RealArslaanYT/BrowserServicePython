bb4239c22ed60994d20e2a3303dc55c5286edc51
Arslaan Pathan <xminecrafterfun@gmail.com>
2025-09-23 10:47:52 +1200

    stupid 3.11 and 3.13 incompatible stuff

--- main.py
+++ main.py
@@ -20,13 +20,10 @@ async def get_or_create_page(sid: str):
     lock = session_locks.setdefault(sid, asyncio.Lock())
     async with lock:
         if sid not in sessions:
-            print(f"creating NEW page {sid}")
             page = await browser.new_page(viewport=ViewportSize(width=1280, height=720))
             await page.goto("https://www.google.com")
             await page.wait_for_load_state("networkidle")
             sessions[sid] = page
-        else:
-            print(f"reusing existing page {sid}, URL: {sessions[sid].url}")
     return sessions[sid]
 
 @app.get("/live")
@@ -59,7 +56,6 @@ async def websocket_endpoint(ws: WebSocket):
         data = json.loads(message)
         if data["type"] == "navigate":
             url = data["url"]
-            print(f"Navigating SID {sid} to {url}")
             if ":/" not in url:
                 url = "http://{url}"
             await page.goto(url)
@@ -67,18 +63,14 @@ async def websocket_endpoint(ws: WebSocket):
         elif data["type"] == "mousemove":
             scaled_x = data["x"] * (1280 / data["img_width"])
             scaled_y = data["y"] * (720 / data["img_height"])
-            print(f"mousemove to {scaled_x}, {scaled_y}")
             await page.mouse.move(scaled_x, scaled_y)
         elif data["type"] == "click":
             scaled_x = data["x"] * (1280 / data["img_width"])
             scaled_y = data["y"] * (720 / data["img_height"])
-            print(f"mouseclick at {scaled_x}, {scaled_y}")
             await page.mouse.click(scaled_x, scaled_y)
         elif data["type"] == "keypress":
-            print(f"keypress with key {data["key"]}")
             await page.keyboard.press(data["key"])
         elif data["type"] == "wheel":
-            print(f"wheel with deltaX {data["deltaX"]}, deltaY {data["deltaY"]}")
             await page.mouse.wheel(data["deltaX"], data["deltaY"])
 
     await page.close()
